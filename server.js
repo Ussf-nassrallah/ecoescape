af44f97ae7e9aca7315abb06734fb38d8cc07f2f
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-03-19 20:54:00 +0000

    Connecting our DB && Creating a Tour Model

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+dotenv.config({ path: './config.env' });
+
 const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connect successfully'))
+  .catch((err) => console.log(`DB connection error: ${err}`));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 /**
  * Start server
