d22ce0e6d72cfc03db52851d5b64e1d5526e30cd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-03-23 19:59:27 +0000

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -24,6 +24,18 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+/**
+ * handling unhandled Routes
+ * all: target all HTTP methods (GET, POST, PUT, PATCH, DELETE)
+ * (*): any endpoint don't defined
+ */
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
 
 // 63
