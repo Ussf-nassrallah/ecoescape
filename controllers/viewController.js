ecba1c7255351c483f6a7c3349b9e4cc784d702b
Ussf-nassrallah <nassrallah.dev@gmail.com>
2024-05-20 20:26:20 +0100

    Build: Tour Overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,15 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // get tours data from collection
+  const tours = await Tour.find();
+  // render overview template by using tours data
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTourDetail = (req, res) => {
   res.status(200).render('tour', {
